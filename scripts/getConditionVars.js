72de070158db3268762f3aab0e61c1a121ef189a
Yeela Lifshitz <yeelalifshitz@Yeela-MacBook-Pro.local>
2024-08-14 17:31:13 +0300

    LINBEE-9077 - add the ability to skip clone + download artifact

---
+++ scripts/getConditionVars.js
@@ -0,0 +1,13 @@
+
+
+module.exports = core => {
+    const CLIENT_PAYLOAD_ARG = process.env.CLIENT_PAYLOAD_ARG;
+    try {
+        const payload = JSON.parse(CLIENT_PAYLOAD_ARG);
+        const isNonCommitEvent = payload.isNonCommitEvent || false;
+        core.exportVariable('IS_NON_COMMIT_EVENT', isNonCommitEvent);  
+    } catch (error) {
+        core.setFailed(error.message);
+        core.exportVariable('IS_NON_COMMIT_EVENT', false);  
+    }
+  };
