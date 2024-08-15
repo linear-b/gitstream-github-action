5fd7dd35838bda605a96006aa7d1933d5c88be4d
Yeela Lifshitz <yeelalifshitz@Yeela-MacBook-Pro.local>
2024-08-15 10:54:02 +0300

    simulate cache

--- scripts/getConditionVars.js
+++ scripts/getConditionVars.js
@@ -4,7 +4,7 @@ module.exports = core => {
     const CLIENT_PAYLOAD_ARG = process.env.CLIENT_PAYLOAD_ARG;
     try {
         const payload = JSON.parse(CLIENT_PAYLOAD_ARG);
-        const isNonCommitEvent = payload.isNonCommitEvent || false;
+        const isNonCommitEvent = 'true'; //payload.isNonCommitEvent || false;
         core.exportVariable('IS_NON_COMMIT_EVENT', isNonCommitEvent);  
     } catch (error) {
         core.setFailed(error.message);
