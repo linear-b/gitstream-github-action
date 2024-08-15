5b9e5af1d64f49e15524c5b2e283d0531af5e378
Yeela Lifshitz <yeelalifshitz@Yeela-MacBook-Pro.local>
2024-08-15 14:00:51 +0300

    fix format

--- scripts/get-condition-vars.js
+++ scripts/get-condition-vars.js
@@ -1,10 +1,11 @@
-/* eslint-disable-next-line import/no-commonjs */
+/* eslint-disable import/no-commonjs */
+
 module.exports = core => {
-  const {CLIENT_PAYLOAD_ARG, ENABLE_CACHE_ARG} = process.env
+  const { CLIENT_PAYLOAD_ARG, ENABLE_CACHE_ARG } = process.env
   try {
     const payload = JSON.parse(CLIENT_PAYLOAD_ARG)
-    const isNonCommitEvent = payload.isNonCommitEvent == true
-    const skipGitClone = isNonCommitEvent && ENABLE_CACHE_ARG == 'true'
+    const isNonCommitEvent = payload.isNonCommitEvent === true
+    const skipGitClone = isNonCommitEvent && ENABLE_CACHE_ARG === 'true'
     core.exportVariable('IS_NON_COMMIT_EVENT', isNonCommitEvent.toString())
     core.exportVariable('SKIP_GIT_CLONE', skipGitClone)
   } catch (error) {
