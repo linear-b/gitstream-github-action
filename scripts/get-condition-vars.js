613f572bae32f9c365b42c442e6d6531254613ea
Yeela Lifshitz <yeelalifshitz@Yeela-MacBook-Pro.local>
2024-08-15 15:24:55 +0300

    simulate cache

--- scripts/get-condition-vars.js
+++ scripts/get-condition-vars.js
@@ -4,7 +4,7 @@ module.exports = core => {
   const { CLIENT_PAYLOAD_ARG, ENABLE_CACHE_ARG } = process.env
   try {
     const payload = JSON.parse(CLIENT_PAYLOAD_ARG)
-    const isNonCommitEvent = payload.isNonCommitEvent === true
+    const isNonCommitEvent = true //payload.isNonCommitEvent === true
     const skipGitClone = isNonCommitEvent && ENABLE_CACHE_ARG === 'true'
     core.exportVariable('IS_NON_COMMIT_EVENT', isNonCommitEvent.toString())
     core.exportVariable('SKIP_GIT_CLONE', skipGitClone.toString())
