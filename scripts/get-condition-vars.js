4becb640fa576f57268c6dfe3433bc5efee03334
Yeela Lifshitz <yeelalifshitz@Yeela-MacBook-Pro.local>
2024-08-18 16:35:08 +0300

    debug

--- scripts/get-condition-vars.js
+++ scripts/get-condition-vars.js
@@ -4,6 +4,7 @@ module.exports = core => {
   const { CLIENT_PAYLOAD_ARG, ENABLE_CACHE_ARG } = process.env
   try {
     const payload = JSON.parse(CLIENT_PAYLOAD_ARG)
+    console.log(`yeela-debug get-condtion-vars types: payload.isNonCommitEvent type: ${typeof(payload.isNonCommitEvent)}, ENABLE_CACHE_ARG type: ${typeof(ENABLE_CACHE_ARG)}`, CLIENT_PAYLOAD_ARG, ENABLE_CACHE_ARG)
     const isNonCommitEvent = payload.isNonCommitEvent === true
     const skipGitClone = isNonCommitEvent && ENABLE_CACHE_ARG === true
     core.exportVariable('IS_NON_COMMIT_EVENT', isNonCommitEvent.toString())
