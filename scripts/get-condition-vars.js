158c32bd59035c8305ba08e90c55b6043d00805f
Yeela Lifshitz <52451294+yeelali14@users.noreply.github.com>
2024-08-18 17:24:09 +0300

    LINBEE-9076 - fix condition (#112)
    * LINBEE-9076 - fix condition
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * fix
    
    * remove log
    
    * format
    
    ---------
    
    Co-authored-by: Yeela Lifshitz <yeelalifshitz@Yeela-MacBook-Pro.local>

--- scripts/get-condition-vars.js
+++ scripts/get-condition-vars.js
@@ -1,15 +1,16 @@
 /* eslint-disable import/no-commonjs */
 
 module.exports = core => {
-  const { CLIENT_PAYLOAD_ARG, ENABLE_CACHE_ARG } = process.env
+  const { IS_NON_COMMIT_ARG, ENABLE_CACHE_ARG } = process.env
   try {
-    const payload = JSON.parse(CLIENT_PAYLOAD_ARG)
-    const isNonCommitEvent = payload.isNonCommitEvent === true
-    const skipGitClone = isNonCommitEvent && ENABLE_CACHE_ARG === 'true'
-    core.exportVariable('IS_NON_COMMIT_EVENT', isNonCommitEvent.toString())
+    const skipGitClone =
+      IS_NON_COMMIT_ARG === 'true' && ENABLE_CACHE_ARG === 'true'
+
+    core.exportVariable('IS_NON_COMMIT_EVENT', IS_NON_COMMIT_ARG)
     core.exportVariable('SKIP_GIT_CLONE', skipGitClone.toString())
   } catch (error) {
     core.setFailed(error.message)
+
     core.exportVariable('IS_NON_COMMIT_EVENT', 'false')
     core.exportVariable('SKIP_GIT_CLONE', 'false')
   }
