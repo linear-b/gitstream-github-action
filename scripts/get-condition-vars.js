2fbfa4678d748aa6788cc44ea3f2e20be4b1cdee
Yeela Lifshitz <yeelalifshitz@Yeela-MacBook-Pro.local>
2024-08-19 12:30:53 +0300

    LINBEE-10041 - fix case when download artifact fails

--- scripts/get-condition-vars.js
+++ scripts/get-condition-vars.js
@@ -1,10 +1,12 @@
 /* eslint-disable import/no-commonjs */
 
 module.exports = core => {
-  const { IS_NON_COMMIT_ARG, ENABLE_CACHE_ARG } = process.env
+  const { IS_NON_COMMIT_ARG, ENABLE_CACHE_ARG, RUN_ID_ARG } = process.env
   try {
+    core.info(`RUN_ID=${RUN_ID}. type: ${typeof RUN_ID}`)
+    const isRunIdExists = RUN_ID_ARG !== 'null'
     const skipGitClone =
-      IS_NON_COMMIT_ARG === 'true' && ENABLE_CACHE_ARG === 'true'
+      IS_NON_COMMIT_ARG === 'true' && ENABLE_CACHE_ARG === 'true' && isRunIdExists
 
     core.exportVariable('IS_NON_COMMIT_EVENT', IS_NON_COMMIT_ARG)
     core.exportVariable('SKIP_GIT_CLONE', skipGitClone.toString())
