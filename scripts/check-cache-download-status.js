9382e561297da01f9c86a1e8efb733f8a8e686d6
Yeela Lifshitz <yeelalifshitz@Yeela-MacBook-Pro.local>
2024-08-19 13:36:49 +0300

    fix format

--- scripts/check-cache-download-status.js
+++ scripts/check-cache-download-status.js
@@ -1,18 +1,17 @@
 /* eslint-disable import/no-commonjs */
 
 module.exports = core => {
-    try {
-      const { OUTCOME_ARG } = process.env
-      core.info(`Setting cache download status to: ${OUTCOME_ARG}`)
-  
-      if (OUTCOME_ARG === 'failure') {
-        core.exportVariable('CACHE_DOWNLOAD_FAILED', 'true')
-      } else {
-        core.exportVariable('CACHE_DOWNLOAD_FAILED', 'false')
-      }
-    } catch (error) {
-      core.setFailed(`Failed to set cache download status: ${error.message}`)
+  try {
+    const { OUTCOME_ARG } = process.env
+    core.info(`Setting cache download status to: ${OUTCOME_ARG}`)
+
+    if (OUTCOME_ARG === 'failure') {
+      core.exportVariable('CACHE_DOWNLOAD_FAILED', 'true')
+    } else {
       core.exportVariable('CACHE_DOWNLOAD_FAILED', 'false')
     }
+  } catch (error) {
+    core.setFailed(`Failed to set cache download status: ${error.message}`)
+    core.exportVariable('CACHE_DOWNLOAD_FAILED', 'false')
   }
-  
+}
