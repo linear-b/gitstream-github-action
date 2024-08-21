8255b8da2dbf823abb4ffac0065be35f370c865a
Yeela Lifshitz <yeelalifshitz@Yeela-MacBook-Pro.local>
2024-08-21 13:56:50 +0300

    format

--- scripts/check-cache-download-status.js
+++ scripts/check-cache-download-status.js
@@ -3,10 +3,17 @@
 module.exports = core => {
   try {
     const { ARTIFACT_OUTCOME_ARG, ARTIFACT_OUTCOME_CM_REPO_ARG } = process.env
-    core.info(`ARTIFACT_OUTCOME_ARG Cache download status: ${ARTIFACT_OUTCOME_ARG}.`)
-    core.info(`ARTIFACT_OUTCOME_CM_REPO_ARG Cache download status: ${ARTIFACT_OUTCOME_CM_REPO_ARG}.`)
+    core.info(
+      `ARTIFACT_OUTCOME_ARG Cache download status: ${ARTIFACT_OUTCOME_ARG}.`
+    )
+    core.info(
+      `ARTIFACT_OUTCOME_CM_REPO_ARG Cache download status: ${ARTIFACT_OUTCOME_CM_REPO_ARG}.`
+    )
 
-    if (ARTIFACT_OUTCOME_ARG === 'failure' || ARTIFACT_OUTCOME_CM_REPO_ARG === 'failure') {
+    if (
+      ARTIFACT_OUTCOME_ARG === 'failure' ||
+      ARTIFACT_OUTCOME_CM_REPO_ARG === 'failure'
+    ) {
       core.exportVariable('CACHE_DOWNLOAD_FAILED', 'true')
     } else {
       core.exportVariable('CACHE_DOWNLOAD_FAILED', 'false')
