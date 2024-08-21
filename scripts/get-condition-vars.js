288923b42598da87d8c377d327ce6d3a7512145a
Yeela Lifshitz <yeelalifshitz@Yeela-MacBook-Pro.local>
2024-08-21 14:50:20 +0300

    debug

--- scripts/get-condition-vars.js
+++ scripts/get-condition-vars.js
@@ -3,14 +3,17 @@
 module.exports = core => {
   const { IS_NON_COMMIT_ARG, ENABLE_CACHE_ARG, RUN_ID_ARG } = process.env
   try {
+    core.info(`RUN_ID_ARG value ${RUN_ID_ARG}. type: ${ typeof RUN_ID_ARG }`) 
     const isRunIdExists = !!RUN_ID_ARG
+    core.info(`isRunIdExists value ${isRunIdExists}. type: ${ typeof RUN_ID_ARG }`) 
     const skipGitClone =
       IS_NON_COMMIT_ARG === 'true' &&
       ENABLE_CACHE_ARG === 'true' &&
       isRunIdExists
 
+    core.info(`skipGitClone value ${skipGitClone}. type: ${ typeof skipGitClone }`) 
     core.info(
-      `IS_NON_COMMIT_ARG type: ${typeof IS_NON_COMMIT_ARG}. ENABLE_CACHE_ARG type: ${typeof ENABLE_CACHE_ARG}.`
+      `IS_NON_COMMIT_ARG type: ${typeof IS_NON_COMMIT_ARG}. ENABLE_CACHE_ARG type: ${typeof ENABLE_CACHE_ARG}. isRunIdExists`
     )
 
     core.exportVariable('IS_NON_COMMIT_EVENT', IS_NON_COMMIT_ARG)
