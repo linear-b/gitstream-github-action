8330ca19415198a19453adead003df34b216b0bf
Yeela Lifshitz <52451294+yeelali14@users.noreply.github.com>
2024-08-26 11:13:21 +0300

    LINBEE-10093 - distinct between org and repo levels on download artifact (#121)
    * LINBEE-10093 - distinct between org and repo levels when download artifact
    
    * fix
    
    * format
    
    * debug
    
    * debug
    
    * debug
    
    * fix
    
    * core debug
    
    * revert debug core
    
    * debug core
    
    * debug core
    
    * debug core
    
    * remove debugs
    
    * abort debug core version
    
    * dedug core
    
    * abort debug core
    
    ---------
    
    Co-authored-by: Yeela Lifshitz <yeelalifshitz@Yeela-MacBook-Pro.local>

--- scripts/check-cache-download-status.js
+++ scripts/check-cache-download-status.js
@@ -2,10 +2,18 @@
 
 module.exports = core => {
   try {
-    const { ARTIFACT_OUTCOME_ARG } = process.env
-    core.info(`Setting cache download status to: ${ARTIFACT_OUTCOME_ARG}`)
+    const { ARTIFACT_OUTCOME_ARG, ARTIFACT_OUTCOME_CM_REPO_ARG } = process.env
+    core.info(
+      `ARTIFACT_OUTCOME_ARG Cache download status: ${ARTIFACT_OUTCOME_ARG}.`
+    )
+    core.info(
+      `ARTIFACT_OUTCOME_CM_REPO_ARG Cache download status: ${ARTIFACT_OUTCOME_CM_REPO_ARG}.`
+    )
 
-    if (ARTIFACT_OUTCOME_ARG === 'failure') {
+    if (
+      ARTIFACT_OUTCOME_ARG === 'failure' ||
+      ARTIFACT_OUTCOME_CM_REPO_ARG === 'failure'
+    ) {
       core.exportVariable('CACHE_DOWNLOAD_FAILED', 'true')
     } else {
       core.exportVariable('CACHE_DOWNLOAD_FAILED', 'false')
