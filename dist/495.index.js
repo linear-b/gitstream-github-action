4b5c70bb07937d9e73794099f5b269c9e42ba205
Elad Kohavi <elad.kohavi@linearb.io>
2024-09-22 11:32:47 +0300

    local teting

---
+++ dist/495.index.js
@@ -0,0 +1,25 @@
+exports.id = 495;
+exports.ids = [495];
+exports.modules = {
+
+/***/ 70495:
+/***/ ((module) => {
+
+function webpackEmptyAsyncContext(req) {
+	// Here Promise.resolve().then() is used instead of new Promise() to prevent
+	// uncaught exception popping up in devtools
+	return Promise.resolve().then(() => {
+		var e = new Error("Cannot find module '" + req + "'");
+		e.code = 'MODULE_NOT_FOUND';
+		throw e;
+	});
+}
+webpackEmptyAsyncContext.keys = () => ([]);
+webpackEmptyAsyncContext.resolve = webpackEmptyAsyncContext;
+webpackEmptyAsyncContext.id = 70495;
+module.exports = webpackEmptyAsyncContext;
+
+/***/ })
+
+};
+;
