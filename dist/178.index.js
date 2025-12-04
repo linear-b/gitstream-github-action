6341fe44ccbbaa97391b39883ce89dee04431773
Misha Kav <misha.kav@gmail.com>
2025-12-04 09:37:19 +0200

    refactor: remove support for Python installation and update dependencies

---
+++ dist/178.index.js
@@ -0,0 +1,16 @@
+exports.id = 178;
+exports.ids = [178];
+exports.modules = {
+
+/***/ 28178:
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+const MemoryStorage = __webpack_require__(81534);
+
+module.exports = MemoryStorage;
+
+
+/***/ })
+
+};
+;
